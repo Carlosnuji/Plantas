2f223e71559586d69b7b92b9e654f92466460111
Carlos <carlosnunez@estudiante.edib.es>
2020-01-09 10:26:18 +0100

    Create a basic server

--- main.cpp
+++ main.cpp
@@ -7,8 +7,56 @@
 int main()
 {
 
+    //Crea un servidor
+    ix::WebSocketServer server(9990, "0.0.0.0");
 
+    server.setOnConnectionCallback(
+                [&server](std::shared_ptr<ix::WebSocket> webSocket,
+                          std::shared_ptr<ix::ConnectionState> connectionState)
+    {
 
-    return 0;
+        webSocket->setOnMessageCallback(
+            [webSocket, connectionState, &server](const ix::WebSocketMessagePtr msg)
+            {
+
+                if (msg->type == ix::WebSocketMessageType::Open)
+                {
+
+                    std::cout << "New connection" << std::endl;
+
+                }
+                else if (msg->type == ix::WebSocketMessageType::Close)
+                {
+                    std::cout << "Bye bye connection" << std::endl;
+                }
+                else if (msg->type == ix::WebSocketMessageType::Message)
+                {
+                    if(!msg->binary)
+                    {
+                        std::cout << msg->str << std::endl;
+                    }
+
+                }
+                else if (msg->type == ix::WebSocketMessageType::Error)
+                {
+                    std::cout << "Error: " << msg->errorInfo.reason << std::endl;
+                }
+
+            }
+        );
+
+    }
+    );
+
+    auto res = server.listen();
+
+    if(!res.first)
+    {
+        return 1;
+    }
+
+    server.start();
+    server.wait();
+    server.stop();
 
 }
